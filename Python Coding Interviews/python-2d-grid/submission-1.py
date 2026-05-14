bdf95cacf86057702417fdf627a8af4697c47af1
Jeongho Suh <43588796+w25536@users.noreply.github.com>
2026-05-15 01:55:17 +0900

    Add: python-2d-grid - submission-1

---
+++ Python Coding Interviews/python-2d-grid/submission-1.py
@@ -0,0 +1,34 @@
+from typing import List
+
+
+def in_bounds(grid: List[List[int]], r: int, c: int) -> bool:
+
+    if r < 0:
+        return False
+    
+    if r >= len(grid):
+        return False
+    
+    if c < 0:
+        return False 
+    
+    if c >= len(grid[0]):
+        return False 
+    
+    return True
+
+   
+
+
+
+    
+
+
+# do not modify below this line
+print(in_bounds([[1, 2, 3], [4, 5, 6], [7, 8, 9]], 0, 0))
+print(in_bounds([[1, 2, 3], [4, 5, 6], [7, 8, 9]], 2, 2))
+print(in_bounds([[1, 2, 3], [4, 5, 6], [7, 8, 9]], 1, 1))
+print(in_bounds([[1, 2, 3], [4, 5, 6], [7, 8, 9]], 4, 3))
+print(in_bounds([[1, 2, 3], [4, 5, 6], [7, 8, 9]], 3, 4))
+print(in_bounds([[1, 2, 3], [4, 5, 6], [7, 8, 9]], 3, -1))
+print(in_bounds([[1, 2, 3], [4, 5, 6], [7, 8, 9]], -1, 3))
